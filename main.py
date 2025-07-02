3809a98859b0a7a8c8fbacb4e9899c303dbe1b82
Imran Hossen <mu.imran2002@gmail.com>
2025-07-02 14:14:41 +0600

    URL Change

--- main.py
+++ main.py
@@ -121,7 +121,3 @@ async def record_vote(vote: Vote):
     )
     
     return {"message": "Vote recorded successfully"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
